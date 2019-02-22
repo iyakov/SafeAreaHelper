fd172460d82e2861182a82c64f8c05489fe2a6b1
Yahor Kleban <11egork@gmail.com>
2019-02-23 00:02:06 +0300

    Adds unity and Xcode stub projects

---
+++ Xcode/SafeAreaHelper/SafeAreaHelper/SafeAreaHelper.h
@@ -0,0 +1,13 @@
+//
+//  SafeAreaHelper.h
+//  SafeAreaHelper
+//
+//  Created by Yahor on 2/22/19.
+//  Copyright © 2019 Yahor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SafeAreaHelper : NSObject
+
+@end
---
+++ Xcode/SafeAreaHelper/SafeAreaHelper/SafeAreaHelper.m
@@ -0,0 +1,13 @@
+//
+//  SafeAreaHelper.m
+//  SafeAreaHelper
+//
+//  Created by Yahor on 2/22/19.
+//  Copyright © 2019 Yahor. All rights reserved.
+//
+
+#import "SafeAreaHelper.h"
+
+@implementation SafeAreaHelper
+
+@end
