c099c092579b36d40709818e6d5f34ec0c854f90
Yahor <11egork@gmail.com>
2019-02-25 17:49:15 +0300

    Adds export script

---
+++ Unity/SafeAreaHelper/Assets/Editor/ExportSafeAreaHelperPackage.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// Creates a neat package for SafeAreaHelper plugins
+/// </summary>
+public static class ExportSafeAreaHelperPackage
+{
+    [MenuItem(">> Package Export Automation <</Export project as Package Now!")]
+    private static void Export()
+    {
+        EditorUtility.DisplayProgressBar("Export package", "Collecting files...", 0.1f);
+
+        try
+        {
+            string[] includePaths = {
+                "Assets/Plugins",
+                "Assets/Demo.unity" 
+            };
+
+            EditorUtility.DisplayProgressBar($"Export package v{Application.version}...", "Exporting...", 0.3f);
+
+            string packageName = $"Builds/SafeAreaHelper.{Application.version}.unitypackage";
+            AssetDatabase.ExportPackage(includePaths, packageName, ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse);
+        }
+        catch(Exception exception)
+        {
+            EditorUtility.DisplayDialog("Error creating a package!", exception.ToString(), "OK");
+        }
+
+        EditorUtility.ClearProgressBar();
+    }
+}
